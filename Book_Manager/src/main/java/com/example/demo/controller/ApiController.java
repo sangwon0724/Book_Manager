bdff265e66945dd1fea94c2069c1baea024a67f3
sangwon0724 <leews0724@naver.com>
2021-11-08 19:54:28 +0900

    Studying Spring - JPA Repository

--- Book_Manager/src/main/java/com/example/demo/controller/ApiController.java
+++ Book_Manager/src/main/java/com/example/demo/controller/ApiController.java
@@ -3,29 +3,33 @@ package com.example.demo.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.demo.entity.BookEntity;
-import com.example.demo.repository.BookRepository;
+import com.example.demo.service.BookService;
 
 @RestController
 public class ApiController {
 	@Autowired
-	private BookRepository bookRepository;
+	private BookService bookService;
 	
 	@GetMapping("/add")
-	public BookEntity add(BookEntity bookVo) {
-		BookEntity bookData = bookRepository.save(bookVo);
-		return bookData;
+	public BookEntity add(BookEntity book) {
+		return bookService.add(book);
 	}
 
 	@GetMapping("/list")
 	public List<BookEntity> list(Model model) {
 
-		List<BookEntity> bookList = bookRepository.findAll();
-
-		return bookList;
+		return bookService.list();
 	}
+	
+	/*@GetMapping("/list")
+	public Page<BookEntity> list(@PageableDefault(size=2, sort="id", direction = Sort.Direction.DESC) Pageable pageable) {
+		return bookService.list(pageable);
+	}*/
 }
---
+++ Book_Manager/src/main/java/com/example/demo/service/BookService.java
@@ -0,0 +1,30 @@
+package com.example.demo.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.example.demo.entity.BookEntity;
+import com.example.demo.repository.BookRepository;
+
+@Service
+public class BookService {
+	@Autowired
+	private BookRepository bookRepository;
+	
+	//저장
+	public BookEntity add(BookEntity book) {
+		return bookRepository.save(book);
+	}
+	
+	public List<BookEntity> list(){
+		//org.springframework.data.domain.Sort & org.springframework.data.domain.Sort.Direction ▼
+		List<BookEntity> bookList = bookRepository.findAll(Sort.by(Direction.DESC,"name"));
+		
+		return bookList;
+	}
+	/*public Page<BookEntity> list(Pageable pageable){ Page<BookEntity> bookList = bookRepository.findAll(pageable); return bookList; }*/
+}
