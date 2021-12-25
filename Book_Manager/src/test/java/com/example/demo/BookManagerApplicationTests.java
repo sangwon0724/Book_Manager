ae76fc51dbdbabfc8e1093e414d127d3766e0327
sangwon0724 <leews0724@naver.com>
2021-12-26 02:11:07 +0900

    Spring Study - Persistence

--- Book_Manager/src/test/java/com/example/demo/BookManagerApplicationTests.java
+++ Book_Manager/src/test/java/com/example/demo/BookManagerApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.example.demo.domain.Gender;
+import com.example.demo.domain.User;
+import com.example.demo.repository.UserRepository;
 
 @SpringBootTest
 class BookManagerApplicationTests {
-
+	@Autowired
+	private UserRepository userRepository;
+	
 	@Test
-	void contextLoads() {
+	void userSaveTest() {
+		User user = new User();
+		user.setName("sangwon");
+		user.setGender(Gender.MALE);
+		
+		userRepository.save(user);
 	}
 
 }
