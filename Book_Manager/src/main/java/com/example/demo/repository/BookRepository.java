b05f560fe85b2951aa8368882abe6ec76d9cea89
sangwon0724 <leews0724@naver.com>
2022-01-09 22:09:05 +0900

    Study Spring - native query

--- Book_Manager/src/main/java/com/example/demo/repository/BookRepository.java
+++ Book_Manager/src/main/java/com/example/demo/repository/BookRepository.java
@@ -9,6 +9,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.demo.domain.Book;
 import com.example.demo.repository.dto.BookNameAndCategory;
@@ -26,16 +27,23 @@ public interface BookRepository extends JpaRepository<Book, Long>{
 
     List<Book> findByCategoryIsNullAndNameEqualsAndCreatedAtGreaterThanEqualAndUpdatedAtGreaterThanEqual(String name, LocalDateTime createdAt, LocalDateTime updatedAt);
 
-    @Query(value = "select b from Book b "
-        + "where name = :name and createdAt >= :createdAt and updatedAt >= :updatedAt and category is null")
-    List<Book> findByNameRecently(
-        @Param("name") String name,
-        @Param("createdAt") LocalDateTime createdAt,
-        @Param("updatedAt") LocalDateTime updatedAt);
+    @Query(value = "select b from Book b where name = :name and createdAt >= :createdAt and updatedAt >= :updatedAt and category is null")
+    List<Book> findByNameRecently(@Param("name") String name, @Param("createdAt") LocalDateTime createdAt, @Param("updatedAt") LocalDateTime updatedAt);
 
     @Query(value = "select new com.fastcampus.jpa.bookmanager.repository.dto.BookNameAndCategory(b.name, b.category) from Book b")
     List<BookNameAndCategory> findBookNameAndCategory();
 
     @Query(value = "select new com.fastcampus.jpa.bookmanager.repository.dto.BookNameAndCategory(b.name, b.category) from Book b")
     Page<BookNameAndCategory> findBookNameAndCategory(Pageable pageable);
+
+    @Query(value = "select * from book", nativeQuery = true)
+    List<Book> findAllCustom();
+
+    @Transactional
+    @Modifying
+    @Query(value = "update book set category = 'IT전문서'", nativeQuery = true)
+    int updateCategories();
+
+    @Query(value = "show tables", nativeQuery = true)
+    List<String> showTables();
 }
