5743f063934e886688a8e4cc128922e03d9a7993
sangwon0724 <leews0724@naver.com>
2022-01-16 21:42:17 +0900

    Study Spring - JPA Trouble Shooting (N+1)

--- Book_Manager/src/main/java/com/example/demo/domain/Comment.java
+++ Book_Manager/src/main/java/com/example/demo/domain/Comment.java
@@ -1,16 +1,10 @@
 package com.example.demo.domain;
 
-import java.util.List;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
-
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -21,26 +15,14 @@ import lombok.ToString;
 @Data
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
-public class Review extends BaseEntity {
+public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String title;
+    private String comment;
 
-    private String content;
-
-    private float score;
-
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne
     @ToString.Exclude
-    private User user;
-
-    @ManyToOne(fetch = FetchType.LAZY)
-    @ToString.Exclude
-    private Book book;
-
-    @OneToMany(fetch = FetchType.LAZY)
-    @JoinColumn(name = "review_id")
-    private List<Comment> comments;
+    private Review review;
 }
--- Book_Manager/src/main/java/com/example/demo/repository/ReviewRepository.java
+++ Book_Manager/src/main/java/com/example/demo/repository/ReviewRepository.java
@@ -1,7 +1,21 @@
 package com.example.demo.repository;
 
 import com.example.demo.domain.Review;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+    @Query("select distinct r from Review r join fetch r.comments")
+    List<Review> findAllByFetchJoin();
+
+    @EntityGraph(attributePaths = "comments")
+    @Query("select r from Review r")
+    List<Review> findAllByEntityGraph();
+
+    @EntityGraph(attributePaths = "comments")
+    List<Review> findAll();
 }
