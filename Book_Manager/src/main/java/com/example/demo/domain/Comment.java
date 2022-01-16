873f2bc6e07b3c2ca4ad240318b8d8c7fa18ff09
sangwon0724 <leews0724@naver.com>
2022-01-16 21:44:58 +0900

    Study Spring - JPA Trouble Shooting (persistence cache)

--- Book_Manager/src/main/java/com/example/demo/domain/Comment.java
+++ Book_Manager/src/main/java/com/example/demo/domain/Comment.java
@@ -1,10 +1,16 @@
 package com.example.demo.domain;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+
+import org.hibernate.annotations.DynamicInsert;
+
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
@@ -15,6 +21,7 @@ import lombok.ToString;
 @Data
 @ToString(callSuper = true)
 @EqualsAndHashCode(callSuper = true)
+@DynamicInsert
 public class Comment extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -25,4 +32,7 @@ public class Comment extends BaseEntity {
     @ManyToOne
     @ToString.Exclude
     private Review review;
+
+    @Column(columnDefinition = "datetime(6) default now(6)")
+    private LocalDateTime commentedAt;
 }
